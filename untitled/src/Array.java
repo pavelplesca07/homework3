d7abcf1d367903cfae1266347b37d9bf0de405f1
pavelplesca <rap2604@mail.ru>
2022-08-05 13:08:16 +0200

    array

---
+++ untitled/src/Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] p = new int [100];
+        p[1]=0;
+
+            System.out.println(p[5]);
+    }
+}
---
+++ untitled/src/Loops.java
@@ -0,0 +1,10 @@
+public class Loops {
+    public static void main(String[] args) {
+
+        int x, y;
+        x = 24;
+        for (y = 0; y <= x; y += 2) {
+            System.out.println(y);
+        }
+    }
+}
